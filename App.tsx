89c4fcc7dcecb1f3702e8fc01ba9802ce5994ba4
Fynn Roberts <robertsfynn@gmail.com>
2019-09-18 15:40:56 +0200

    added styled components and replaced old stylesheets with styled components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Image } from 'react-native';
 import * as Font from 'expo-font';
 import Button from './components/Button';
 import { AppLoading } from 'expo';
+import styled from 'styled-components/native';
 
 interface State {
   isReady: boolean;
 }
+
+const StyledContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const StyledHeader = styled.Text`
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+  margin-top: 50px;
+  margin-bottom: 10px;
+`;
+
+const StyledDescription = styled.Text`
+  font-size: 15px;
+  line-height: 20px;
+  text-align: center;
+  padding: 0 40px;
+`;
+
+const StyledTextContainer = styled.View`
+  margin-bottom: 150px;
+`;
 
 export default class App extends Component<{}, State> {
   state = {
@@ -36,46 +64,20 @@ export default class App extends Component<{}, State> {
     }
 
     return (
-      <View style={styles.container}>
+      <StyledContainer>
         <Image
           source={require('./assets/img.png')}
           style={{ width: 225, height: 175 }}
         />
-        <View style={styles.text}>
-          <Text style={styles.title}>Want our advice?</Text>
-          <Text style={styles.description}>
+        <StyledTextContainer>
+          <StyledHeader>Want our advice?</StyledHeader>
+          <StyledDescription>
             To give you best experience we would like to ask a few quick
             questions to set everything up for you.
-          </Text>
-        </View>
+          </StyledDescription>
+        </StyledTextContainer>
         <Button text="Let's do it" onPress={() => console.log('CLICK')} />
-      </View>
+      </StyledContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  text: {
-    marginBottom: 150,
-  },
-  title: {
-    fontSize: 24,
-    lineHeight: 28,
-    textAlign: 'center',
-    marginTop: 50,
-    marginBottom: 10,
-  },
-  description: {
-    fontSize: 15,
-    lineHeight: 20,
-    textAlign: 'center',
-    paddingHorizontal: 40,
-  },
-});
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,39 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import styled from 'styled-components/native';
 
 interface Props {
   text: string;
   onPress: () => void;
 }
 
+const StyledButtonText = styled.Text`
+  font-family: 'Rubik-Medium';
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 15px;
+  line-height: 18px;
+  text-align: center;
+  letter-spacing: 2px;
+`;
+
 const Button = ({ text, onPress }: Props) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <View>
-        <LinearGradient
-          colors={['#23253A', '#5063EE']}
-          style={{ paddingHorizontal: 50, paddingVertical: 25 }}
-          start={{ x: 1, y: 1 }}
-          end={{ x: 0, y: 0 }}
-          locations={[0.0, 0.79]}
-        >
-          <Text style={styles.text}>{text}</Text>
-        </LinearGradient>
-      </View>
+      <LinearGradient
+        colors={['#23253A', '#5063EE']}
+        style={{ paddingHorizontal: 50, paddingVertical: 25 }}
+        start={{ x: 1, y: 1 }}
+        end={{ x: 0, y: 0 }}
+        locations={[0.0, 0.79]}
+      >
+        <StyledButtonText>{text}</StyledButtonText>
+      </LinearGradient>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
-  text: {
-    fontFamily: 'Rubik-Medium',
-    color: 'white',
-    textTransform: 'uppercase',
-    fontSize: 15,
-    lineHeight: 18,
-    textAlign: 'center',
-    letterSpacing: 2,
-  },
-});
 export default Button;
