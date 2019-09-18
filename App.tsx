6427cafa70638d047f87494f6b15ceffd1f369a3
Fynn Roberts <robertsfynn@gmail.com>
2019-09-18 16:11:42 +0200

    adjusted folder structure

--- App.tsx
+++ App.tsx
@@ -1,40 +1,11 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
 import * as Font from 'expo-font';
-import Button from './components/Button';
 import { AppLoading } from 'expo';
-import styled from 'styled-components/native';
+import Greeting from './views/signup/greeting';
 
 interface State {
   isReady: boolean;
 }
-
-const StyledContainer = styled.View`
-  flex: 1;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
-  padding: 20px;
-`;
-
-const StyledHeader = styled.Text`
-  font-size: 24px;
-  line-height: 28px;
-  text-align: center;
-  margin-top: 50px;
-  margin-bottom: 10px;
-`;
-
-const StyledDescription = styled.Text`
-  font-size: 15px;
-  line-height: 20px;
-  text-align: center;
-  padding: 0 40px;
-`;
-
-const StyledTextContainer = styled.View`
-  margin-bottom: 150px;
-`;
 
 export default class App extends Component<{}, State> {
   state = {
@@ -56,28 +27,12 @@ export default class App extends Component<{}, State> {
           startAsync={this.cacheResourcesAsync}
           onFinish={() => {
             this.setState({ isReady: true });
-            console.log('done');
           }}
           onError={console.warn}
         />
       );
     }
 
-    return (
-      <StyledContainer>
-        <Image
-          source={require('./assets/img.png')}
-          style={{ width: 225, height: 175 }}
-        />
-        <StyledTextContainer>
-          <StyledHeader>Want our advice?</StyledHeader>
-          <StyledDescription>
-            To give you best experience we would like to ask a few quick
-            questions to set everything up for you.
-          </StyledDescription>
-        </StyledTextContainer>
-        <Button text="Let's do it" onPress={() => console.log('CLICK')} />
-      </StyledContainer>
-    );
+    return <Greeting />;
   }
 }
---
+++ views/signup/greeting.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Button from '../../components/Button';
+import styled from 'styled-components/native';
+
+const StyledContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const StyledHeader = styled.Text`
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+  margin-top: 50px;
+  margin-bottom: 10px;
+`;
+
+const StyledDescription = styled.Text`
+  font-size: 15px;
+  line-height: 20px;
+  text-align: center;
+  padding: 0 40px;
+`;
+
+const StyledTextContainer = styled.View`
+  margin-bottom: 150px;
+`;
+
+const Greeting = () => {
+  return (
+    <StyledContainer>
+      <Image
+        source={require('./assets/img.png')}
+        style={{ width: 225, height: 175 }}
+      />
+      <StyledTextContainer>
+        <StyledHeader>Want our advice?</StyledHeader>
+        <StyledDescription>
+          To give you best experience we would like to ask a few quick questions
+          to set everything up for you.
+        </StyledDescription>
+      </StyledTextContainer>
+      <Button text="Let's do it" onPress={() => console.log('CLICK')} />
+    </StyledContainer>
+  );
+};
+
+export default Greeting;
