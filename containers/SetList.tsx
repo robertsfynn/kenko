4f46a121045ce1b8aca0d39727ab0f43ffe105a2
Fynn Roberts <robertsfynn@gmail.com>
2019-09-22 11:18:29 +0200

    removed console logs

--- containers/SetList.tsx
+++ containers/SetList.tsx
@@ -6,7 +6,6 @@ import { connect } from 'react-redux';
 
 class SetList extends Component {
   render() {
-    console.log(this.props);
     return (
       <View>
         {this.props.chosenExercises.map((exercise) => (
