5b6826f9c76c6bc19fa747231500a7316b50ba78
Fynn Roberts <robertsfynn@gmail.com>
2019-09-28 18:33:04 +0200

    put info in its own component and added getWorkouts to home

---
+++ components/Info.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const InfoContainer = styled.View`
+  flex: 1;
+  padding: 15px;
+  border: 1px solid #dfdfe6;
+  border-left-width: 0;
+  border-right-width: ${({ noRightBorder }) => (noRightBorder ? 0 : '1px')};
+`;
+
+const InfoTitle = styled.Text`
+  font-family: Questrial;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 29px;
+  line-height: 30px;
+  color: #484856;
+`;
+
+const InfoSubtitle = styled.Text`
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  line-height: 16px;
+  color: rgba(38, 38, 43, 0.7);
+`;
+
+const Info = ({ number, subtitle, unit }) => {
+  return (
+    <InfoContainer>
+      <Text>
+        <InfoTitle>{number} </InfoTitle>
+        <InfoSubtitle>{unit}</InfoSubtitle>
+      </Text>
+      <InfoSubtitle>{subtitle}</InfoSubtitle>
+    </InfoContainer>
+  );
+};
+
+export default Info;
--- views/home/Home.tsx
+++ views/home/Home.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, AsyncStorage } from 'react-native';
 import styled from 'styled-components/native';
 import Button from '../../components/Button';
+import Info from '../../components/Info';
 
 const Title = styled.Text`
   font-family: Rubik-Medium;
@@ -16,53 +17,29 @@ const InfosContainer = styled.View`
   flex-direction: row;
 `;
 
-const Info = styled.View`
-  flex: 1;
-  padding: 15px;
-  border: 1px solid #dfdfe6;
-  border-left-width: 0;
-  border-right-width: ${({ noRightBorder }) => (noRightBorder ? 0 : '1px')};
-`;
-
-const InfoTitle = styled.Text`
-  font-family: Questrial;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 29px;
-  line-height: 30px;
-  /* identical to box height */
-
-  color: #484856;
-`;
-
-const InfoSubtitle = styled.Text`
-  font-family: Rubik;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 13px;
-  line-height: 16px;
-
-  color: rgba(38, 38, 43, 0.7);
-`;
-
 export class Home extends Component {
+  state = {
+    workouts: null,
+  };
+
+  componentDidMount() {
+    this.getWorkouts();
+  }
+
+  getWorkouts = async () => {
+    const workouts = JSON.parse(await AsyncStorage.getItem('workouts')) || [];
+    this.setState({ workouts });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <SafeAreaView>
         <Title>Dashboard</Title>
         <InfosContainer>
-          <Info>
-            <InfoTitle>0</InfoTitle>
-            <InfoSubtitle>workouts completed</InfoSubtitle>
-          </Info>
-          <Info>
-            <InfoTitle>0</InfoTitle>
-            <InfoSubtitle>workouts completed</InfoSubtitle>
-          </Info>
-          <Info noRightBorder={true}>
-            <InfoTitle>0</InfoTitle>
-            <InfoSubtitle>workouts completed</InfoSubtitle>
-          </Info>
+          <Info number={0} subtitle="workouts completed" />
+          <Info number={0} subtitle="workouts completed" unit={'kg'} />
+          <Info number={0} subtitle="workouts completed" />
         </InfosContainer>
         <Button
           text="Create Workout"
